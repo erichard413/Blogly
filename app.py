9359f21a0ed007886dc3461c4b83afbc43e679e3
root <root@Erik-PC.localdomain>
2022-10-28 18:28:28 -0400

    pushing update for part-2

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 """Blogly application."""
 
+
+
 from flask import Flask, render_template, redirect, request
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post, get_posts_by_id, get_post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -30,7 +32,8 @@ def list_users():
 def user_information(user_id):
     """detail of user"""
     user = User.query.get_or_404(user_id)
-    return render_template("user.html", user=user)
+    post = get_posts_by_id(user_id)
+    return render_template("user.html", user=user, post=post)
 
 @app.route("/users/new")
 def add_user():
@@ -82,4 +85,68 @@ def edit_user(user_id):
     db.session.commit()
     return redirect(f'/users/{user.id}')
 
+@app.route("/users/<int:user_id>/posts/new")
+def add_post_form(user_id):
+    """add post form"""
+    user = User.query.get_or_404(user_id)
+    return render_template("addpost.html", user=user)
 
+@app.route("/users/<int:user_id>/posts/new", methods=["POST"])
+def create_post(user_id):
+    """add post form"""
+    title = request.form["title"]
+    content = request.form["content"]
+    new_post = Post(title=title, content=content, user_id=user_id)
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f"/users/{user_id}")
+
+@app.route("/posts/<int:post_id>")
+def show_posts(post_id):
+    post = Post.query.get(post_id)
+    user_id = post.user_id
+    user = User.query.get(user_id)
+    return render_template("post.html", post=post, user=user)
+
+@app.route("/posts/<int:post_id>/edit")
+def edit_post(post_id):
+    post = Post.query.get(post_id)
+    return render_template("editpost.html", post=post)
+
+@app.route("/posts/<int:post_id>/edit", methods=["POST"])
+def update_post(post_id):
+    """add post form"""
+    post = Post.query.get(post_id)
+    user_id = post.user_id
+    post.title = request.form["title"]
+    post.content = request.form["content"]
+    db.session.add(post)
+    db.session.commit()
+    return redirect(f"/posts/{post_id}")
+
+@app.route("/posts/<int:post_id>/delete", methods=["POST"])
+def delete_post(post_id):
+    """delete post from id"""
+    post = Post.query.get(post_id)
+    user_id = post.user_id
+    Post.query.filter_by(id=post_id).delete()
+    db.session.commit()
+    return redirect(f'/users/{user_id}')
+
+
+# Add Post Routes
+# GET /users/[user-id]/posts/new
+# Show form to add a post for that user.
+# POST /users/[user-id]/posts/new
+# Handle add form; add post and redirect to the user detail page.
+# GET /posts/[post-id]
+# Show a post.
+
+# Show buttons to edit and delete the post.
+
+# GET /posts/[post-id]/edit
+# Show form to edit a post, and to cancel (back to user page).
+# POST /posts/[post-id]/edit
+# Handle editing of a post. Redirect back to the post view.
+# POST /posts/[post-id]/delete
+# Delete the post.
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
-
+import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime
 
 db = SQLAlchemy()
 
@@ -21,6 +22,10 @@ class User(db.Model):
                     nullable=False)
     last_name = db.Column(db.String(15), nullable=False)
     image_url = db.Column(db.String, default="/static/images/defaultpic.png")
+    def __repr__(self):
+        """Representation output"""
+        return f"<Post {self.first_name} {self.last_name} {self.img_url}>"
+    
 
     @property
     def full_name(self):
@@ -31,5 +36,33 @@ class User(db.Model):
     def get_full_name(self):
         """gets full name"""
         u = self
-        return f"{u.first_name} {u.last_name}" 
+        return f"{u.first_name} {u.last_name}"
+
+class Post(db.Model):
+    """Post model"""
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    title = db.Column(db.String(50),
+                    nullable=False)
+    content = db.Column(db.String(1000),
+                    nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', backref='posts')
+    def __repr__(self):
+        """Representation output"""
+        return f"<Post {self.title} {self.content} {self.created_at} {self.user_id}>"
+def get_posts_by_id(x):
+    """returns posts with user id"""
+    p = Post.query.filter_by(user_id=x).all()
+    return p
+def get_post(user, id):
+    """returns single post from user with id of id"""
+    p = Post.query.filter_by(user_id=user, id=id)
+    return p
+    
+    
 
